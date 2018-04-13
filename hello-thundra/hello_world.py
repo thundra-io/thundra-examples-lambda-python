6182f01baa3a9ac92284f97504349a66bdd68ef8
fazilet ozer <fazilet@opsgenie.com>
2018-04-13 10:06:05 +0300

    Added hello-thundra & hello-thundra-async examples

---
+++ hello-thundra/hello_world.py
@@ -0,0 +1,10 @@
+from thundra.thundra_agent import Thundra
+
+thundra = Thundra()
+
+@thundra
+def handler(event, context):
+    response = {
+        'message': 'Hello Thundra!'
+    }
+    return response
